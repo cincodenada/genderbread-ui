43a54d9f3be94db1eaf1d3fab007ce28d86c93e6
Joel Bradshaw <cincodenada@gmail.com>
2012-06-24 22:56:47 -0700

    Switching to a more complex document in MongoDB, because I can Implementing cache generation, and optimizing/refactoring the image generator a bit

--- genimage.php
+++ genimage.php
@@ -1,7 +1,21 @@
 <?php 
 require_once('config.php');
 
-function genKey($int) {
+/**
+ * genKey 
+ * Generates a four-character minimum key based on an integer
+ * This is pretty much just zero-padded Base64, but with 52 (26*2)
+ * characters instead.  And it's a-padded, since a is zero.
+ *
+ * The goal is to get a nice-looking, short, unique key to tag onto
+ * URLs to refer to individual genderbreads.
+ * 
+ * @param mixed $int The id number to transform into a key
+ * @param int $padding The number of "digits" to pad to
+ * @access public
+ * @return string A padded key that maps to the given integer
+ */
+function genKey($int, $padding = 3) {
     $letterset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $base = strlen($letterset);
     $out = '';
@@ -12,63 +26,91 @@ function genKey($int) {
         $out .= $letterset[$curpart];
     }
     //Pad to three characters
-    for($curpad = strlen($out); $curpad <= 3; $curpad++) {
+    for($curpad = strlen($out); $curpad <= $padding; $curpad++) {
         $out = "a$out";
     }
     return $out;
 }
 
+//Initialize MongoDB
 $m = new Mongo();
 $db = $m->genderbread;
 $gblist = $db->genderbreads;
 
-$newgb = array(
-    'name' => $_POST['name'],
-);
+$newgb = empty($_GET['key']);
+if($newgb) {
+    $newgb = true;
+    //Pull in the values we want from POST
+    $gbdata = array_intersect_key(
+        $_POST,
+        array_flip(array('name'))
+    );
+    //Fill out the $gbdata array
+    foreach($sliders as $id => $info) {
+        foreach(array('top','bottom') as $which) {
+            $gbdata['handles'][$id][$which] = $_POST["$id-$which"];
+        }
+    }
+} else {
+    $gbdata = $gblist->findOne(array('key' => $_GET['key']));
+}
 
+//Generate the image from the $gbdata array
 $outimg = imagecreatefrompng('images/template/blank.png');
 imagealphablending($outimg, true);
-foreach($sliders as $id => $info) {
+foreach($gbdata['handles'] as $id => $valarr) {
     $handle = imagecreatefrompng("images/template/handle-$id.png");
     imagealphablending($handle, true);
     $hw = imagesx($handle);
     $hh = imagesy($handle);
-    foreach(array('top','bottom') as $num => $which) {
-        $val = $_POST["$id-$which"];
-        $newgb["$id-$which"] = $val;
-
+    $yvals = array_combine(array('top','bottom'),$template_data['y_vals'][$id]);
+    foreach($valarr as $which => $val) {
         $x = $template_data['slider_start'] + ($val/100) * ($template_data['slider_end'] - $template_data['slider_start']);
-        $y = $template_data['y_vals'][$id][$num];
+        $y = $yvals[$which];
         
         imagecopymerge($outimg, $handle, $x,$y, 0,0,$hw,$hh, 100);
     }
     imagedestroy($handle);
 }
 
-$success = false;
-$jump_ahead = 0;
-while($success == false && $jump_ahead < 50) {
-    $jump_ahead++;
-    $link = $gblist->find()->sort(array('index' => -1))->limit(1);
-    $mostrecent = $link->hasNext() ? $link->getNext() : array('index' => 1);
+if($newgb) {
+    //If it was a new genderbread, save it to the db
+    $success = false;
+    $jump_ahead = 0;
+    $gbdata['created'] = time();
+    while($success == false && $jump_ahead < 50) {
+        $jump_ahead++;
+        $link = $gblist->find()->sort(array('index' => -1))->limit(1);
+        $mostrecent = $link->hasNext() ? $link->getNext() : array('index' => 1);
 
-    $newgb['index'] = $mostrecent['index'] + $jump_ahead;
-    $newgb['key'] = genKey($newgb['index']);
-    $status = $gblist->insert($newgb);
-    $success = ($status['err'] == null);
+        $gbdata['index'] = $mostrecent['index'] + $jump_ahead;
+        $gbdata['key'] = genKey($gbdata['index']);
+        $status = $gblist->insert($gbdata);
+        $success = ($status['err'] == null);
+    }
 }
 
-if($success) {
-    $filename = $newgb['key'];
+if($newgb && !$success) {
+    $filename = uniqid('f_'); //Generate a failed-save filename
 } else {
-    $filename = uniqid('f_');
+    $filename = $gbdata['key'];
 }
 
-//header('Content-Type: image/png');
+//Combine and save the image
 imagealphablending($outimg, false);
 imagesavealpha($outimg, true);
 imagepng($outimg, "cache/$filename.png");
-imagedestroy($outimg);
 
-header("Location: gb/$filename");
+if($newgb) {
+    //If it's a new genderbread, redirect them
+    //to their genderbread page
+    imagedestroy($outimg);
+    header("Location: gb/$filename");
+} else {
+    //We could redirect to the image, but might as well just 
+    //dump it here and save a redirect
+    header('Content-Type: image/png');
+    imagepng($outimg);
+    imagedestroy($outimg);
+}
 ?>
